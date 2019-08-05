72558f5889e843b6e670df6d046ce5f8e8aae308
sahanwellala <sahan1996@gmail.com>
2019-08-05 19:24:21 +0530

    Updated the Project Structure

---
+++ project/Release/code/Backend/code-complexity-analyzer/src/main/java/com/neo/codecomplexityanalyzer/CodeComplexityAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.neo.codecomplexityanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeComplexityAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeComplexityAnalyzerApplication.class, args);
+	}
+
+}
