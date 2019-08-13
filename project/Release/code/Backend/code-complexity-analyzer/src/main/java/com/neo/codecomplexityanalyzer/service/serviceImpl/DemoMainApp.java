797ddb8943a74ae8fdd46c4bb60aad94cbac96b8
sahanwellala <sahan1996@gmail.com>
2019-08-13 15:21:39 +0530

    Added the Functions to find the number of Ancestors in a cpp file

--- project/Release/code/Backend/code-complexity-analyzer/src/main/java/com/neo/codecomplexityanalyzer/service/serviceImpl/DemoMainApp.java
+++ project/Release/code/Backend/code-complexity-analyzer/src/main/java/com/neo/codecomplexityanalyzer/service/serviceImpl/DemoMainApp.java
@@ -1,7 +1,5 @@
 package com.neo.codecomplexityanalyzer.service.serviceImpl;
 
-import java.util.HashMap;
-
 public class DemoMainApp {
 	public static void main(String[] args) {
 
@@ -11,21 +9,25 @@ public class DemoMainApp {
 		CiJavaServicesImpl ciJ = new CiJavaServicesImpl(
 				"/home/sahan/Documents/My Documents/SLIIT/SPM/Code-complexity-analyzer/project/Release/code/Backend/code-complexity-analyzer/src/main/resources/sampleData/InheritanceSample.java");
 
-		//ciJ.complexityOfAllClassesDueToInheritance();
-		for (String string : ci.getAncestorClasses("TA")) {
-			System.out.print(string);
-		}
-		
+		// System.out.println(ciJ.calComplexityDueToInheritance("SportsCar"));
+		ci.identifyClassStructure();
+		System.out.println(ci.getNumberOfAncestorClasses("TA"));
+		System.out.println(ci.getAncestorCountMapForAllClasses());
+//		//ciJ.complexityOfAllClassesDueToInheritance();
+//		for (String string : ci.getAncestorClasses("TA")) {
+//			System.out.print(string);
+//		}
+
 //		for (String string : ciJ.getAnsestorClassNames("SportsCar")) {
 //			System.out.println(string);
 //		}
 
-		HashMap<String, String> map = ci.getClassMapping();
-		for (String name : map.keySet()) {
-			String key = name.toString();
-			String value = map.get(name).toString();
-			System.out.println(key + " " + value);
-		}
+//		HashMap<String, String> map = ci.getClassMapping();
+//		for (String name : map.keySet()) {
+//			String key = name.toString();
+//			String value = map.get(name).toString();
+//			System.out.println(key + " " + value);
+//		}
 		// System.out.println("Count Returned : " + count);
 	}
 
